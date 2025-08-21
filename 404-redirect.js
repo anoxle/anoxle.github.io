fdd33021a28f633f3dba582881af5ecfb4af8c80
Anoxle <anoxlemain@gmail.com>
2025-08-21 23:32:06 +0530

    #

--- 404-redirect.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const currentPath = window.location.pathname;
-    
-    if (!currentPath.includes('404') && !window.location.search.includes('url=')) {
-        const currentUrl = window.location.href;
-        window.location.href = '/404?url=' + encodeURIComponent(currentUrl);
-    }
-});
